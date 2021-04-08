7728d419659270c3dd251179fca8f584c4fd7413
Gustavo-tech <gustavosh31@gmail.com>
2021-04-07 22:34:38 -0300

    Overriding validate entity

---
+++ ICar.Data/Utilities/Validations/UserValidator.cs
@@ -0,0 +1,23 @@
+﻿using ICar.Data.Models;
+using ICar.Data.Validations.Abstracts;
+using System.Text.RegularExpressions;
+
+namespace ICar.Data.Utilities.Validations
+{
+    public class UserValidator : EntityValidator<User>
+    {
+        private bool ValidateCpf(string cpf)
+        {
+            string reg = "[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0,9]{3}";
+            return Regex.IsMatch(cpf, reg);
+        }
+
+        public override bool ValidateEntity(User user)
+        {
+            return ValidateName(user.Name) &&
+                   ValidatePassword(user.Password) &&
+                   ValidateCity(user.City) &&
+                   ValidateCpf(user.Cpf);
+        }
+    }
+}
---
+++ ICar.Data/Validations/CompanyValidator.cs
@@ -0,0 +1,23 @@
+﻿using ICar.Data.Models;
+using ICar.Data.Validations.Abstracts;
+using System.Text.RegularExpressions;
+
+namespace ICar.Data.Validations
+{
+    public class CompanyValidator : EntityValidator<Company>
+    {
+        private bool ValidateCnpj(string cnpj)
+        {
+            string pattern = "[0-9]{2}.[0-9]{3}.[0-9]{3}[/][0-9]{4}[-][0-9]{2}";
+            return Regex.IsMatch(cnpj, pattern);
+        }
+
+        public override bool ValidateEntity(Company company)
+        {
+            return ValidateName(company.Name) &&
+                   ValidatePassword(company.Password) &&
+                   ValidateCity(company.City) &&
+                   ValidateCnpj(company.Cnpj);
+        }
+    }
+}
