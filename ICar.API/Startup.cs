efd9fd913949df962eae44ea9276ba92bbdd07c7
Gustavo-tech <gustavosh31@gmail.com>
2021-06-03 10:13:30 -0300

    Fixing get users exception

--- ICar.API/Startup.cs
+++ ICar.API/Startup.cs
@@ -27,7 +27,9 @@ namespace ICar.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options => 
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ICar.API", Version = "v1" });
