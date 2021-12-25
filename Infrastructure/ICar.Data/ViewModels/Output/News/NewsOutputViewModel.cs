2d0e0779f46a12d46d0cbddb4edcd05bee9a4a58
Gustavo-tech <gustavosh31@gmail.com>
2021-12-25 15:02:22 -0300

    small changes

--- Infrastructure/ICar.Data/ViewModels/Output/News/NewsOutputViewModel.cs
+++ Infrastructure/ICar.Data/ViewModels/Output/News/NewsOutputViewModel.cs
@@ -5,17 +5,21 @@ namespace ICar.Infrastructure.ViewModels.Output.News
     public class NewsOutputViewModel
     {
         public string Id { get; private set; }
-        public string Author { get; private set; }
+        public string AuthorId { get; private set; }
         public string Title { get; private set; }
         public string Text { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
 
-        public NewsOutputViewModel(string id, string author, string title,
+        public NewsOutputViewModel()
+        {
+        }
+
+        public NewsOutputViewModel(string id, string authorId, string title, 
             string text, DateTime createdAt, DateTime updatedAt)
         {
             Id = id;
-            Author = author;
+            AuthorId = authorId;
             Title = title;
             Text = text;
             CreatedAt = createdAt;
